196a1e0d52b745acf5482e19e2faaf5e70ef7117
Matheus Felipe <matheusfeliperibas@gmail.com>
2021-09-16 02:13:28 -0300

    config deploy in git pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
 
 import store from './store'
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Calculator from './pages/Calculator'
 import History from './pages/History'
 
 export default function Routes () {
     return (
-        <BrowserRouter basename='https://matheusribas.github.io/calculator'>
-            <Switch>
-                <Route path="/" exact component={Calculator} />
-                <Route path="/history" component={History} />
-            </Switch>
-        </BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Calculator} />
+            <Route path="/history" component={History} />
+        </Switch>
     );
 }
